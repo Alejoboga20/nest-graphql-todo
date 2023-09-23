c11a576a82662cea1ebfc7ebae11651f087788b0
Alejo Boga <alejoboga20@gmail.com>
2023-09-23 16:57:22 -0300

    generate new query

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,9 +1,17 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Float, Query, Resolver } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
-  @Query(() => String)
+  @Query(() => String, { name: 'hello', description: 'Hello World Query' })
   helloWorld(): string {
     return 'Hello World!';
+  }
+
+  @Query(() => Float, {
+    name: 'randomNumber',
+    description: 'Random Number Query',
+  })
+  randomNumber(): number {
+    return Math.random() * 100;
   }
 }
