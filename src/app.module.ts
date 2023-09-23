70df3c70b9172d23d56969b74f0749a451215f15
Alejo Boga <alejoboga20@gmail.com>
2023-09-23 16:51:05 -0300

    use apollo playground

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,8 @@ import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
+
 import { HelloWorldModule } from './hello-world/hello-world.module';
 
 @Module({
@@ -9,6 +11,8 @@ import { HelloWorldModule } from './hello-world/hello-world.module';
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
     }),
     HelloWorldModule,
   ],
